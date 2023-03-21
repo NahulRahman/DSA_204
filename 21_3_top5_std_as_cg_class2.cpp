14f53713577686bf837281f77c3386e93e05efa8
MD. NAHUL RAHMAN <123864673+NahulRahman@users.noreply.github.com>
2023-03-21 16:08:49 +0600

    Add files via upload

---
+++ 21_3_top5_std_as_cg_class2.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+class student{
+public:
+    long long int id;
+    float cg;
+};
+
+int main(){
+    int i,n;
+    cout<<"Enter number of students:";
+    cin>>n;
+    student s[n];
+    float maximum;
+    int b;
+    for(int i=0;i<n;i++){
+        cout<<"Enter ID for student"<<i+1<<": ";
+        cin>>s[i].id;
+        cout<<"Enter CGPA"<<": ";
+        cin>>s[i].cg;
+        if(s[i].cg>maximum){
+                maximum=s[i].cg;
+                b=s[i].id;
+    }
+    }cout<<"The maximum CGPA was : "<<maximum<<", axhieved by student no: "<<b;
+    return 0;
+}
+
+
+
