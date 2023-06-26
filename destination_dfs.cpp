df6267d999a90d951ef150c1523af7ce1c7298a9
MD. NAHUL RAHMAN <123864673+NahulRahman@users.noreply.github.com>
2023-06-26 19:28:37 +0600

    Add files via upload

---
+++ destination_dfs.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+using namespace std;
+
+struct City {
+    string name;
+    string country;
+    bool isCapital;
+
+    City(string name, string country, bool isCapital)
+        : name(name), country(country), isCapital(isCapital) {}
+};
+
+void dfs(const unordered_map<string, vector<string>>& graph, const string& city, const string& start, unordered_map<string, string>& countryMap, unordered_map<string, string>& pathMap, unordered_map<string, int>& distanceMap) {
+    for (const string& neighbor : graph.at(city)) {
+        if (countryMap.find(neighbor) == countryMap.end()) {
+            countryMap[neighbor] = countryMap[city];
+            pathMap[neighbor] = pathMap[city] + " -> " + neighbor;
+            distanceMap[neighbor] = distanceMap[city] + 100;
+            dfs(graph, neighbor, start, countryMap, pathMap, distanceMap);
+        }
+    }
+}
+
+void findShortestPath(const unordered_map<string, vector<string>>& graph, const string& start) {
+    unordered_map<string, string> countryMap;
+    unordered_map<string, string> pathMap;
+    unordered_map<string, int> distanceMap;
+
+    countryMap[start] = graph.at(start)[0];
+    pathMap[start] = start;
+    distanceMap[start] = 0;
+
+    dfs(graph, start, start, countryMap, pathMap, distanceMap);
+
+    for (auto it = countryMap.begin(); it != countryMap.end(); ++it) {
+        if (it->first != start) {
+            cout << it->second << endl;
+            cout << pathMap[it->first] << endl;
+            cout << distanceMap[it->first] << " km" << endl;
+        }
+    }
+}
+
+int main() {
+    int e;
+    cin >> e;
+
+    unordered_map<string, vector<string>> graph;
+
+    for (int i = 0; i < e; i++) {
+        string city1, city2, country;
+        cin >> city1 >> city2 >> country;
+        graph[city1].push_back(city2);
+        graph[city2].push_back(city1);
+        if (city1[0] >= 'A' && city1[0] <= 'Z') {
+            graph[city1].push_back(country);
+            graph[country].push_back(city1);
+        }
+        if (city2[0] >= 'A' && city2[0] <= 'Z') {
+            graph[city2].push_back(country);
+            graph[country].push_back(city2);
+        }
+    }
+
+    int q;
+    cin >> q;
+
+    for (int i = 0; i < q; i++) {
+        string query;
+        cin >> query;
+        if (graph.find(query) != graph.end()) {
+            findShortestPath(graph, query);
+        } else {
+            cout << "City not found" << endl;
+        }
+    }
+
+    return 0;
+}
